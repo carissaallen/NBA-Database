bfb1584f63f4fa8ffb77e8866a1e217f1f04a887
Carissa Wood <carissawood@outlook.com>
2019-03-01 20:59:53 -0800

    Add Top Scorers table

---
+++ SQL/CreateTable_TopScorers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Top_Scorers (
+Points        INT NOT NULL
+,Name         VARCHAR(100) NOT NULL
+,Year         INT
+,TeamName     VARCHAR(100)
+,OppTeamName  VARCHAR(100)
+,TeamScore    INT
+,OppTeamScore INT
+,MinsPlayed   INT);
