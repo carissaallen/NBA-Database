17489779005a07184e9ef6d25fac090435cc2e1d
Carissa Wood <carissawood@outlook.com>
2019-03-01 20:16:13 -0800

    Create Coach_Stats table

---
+++ SQL/CreateTable_CoachStats.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Coach_Stats (
+Name      VARCHAR(100) NOT NULL
+,Team     VARCHAR(10)
+,SeasG    INT
+,SeasW    INT
+,SeasL    INT
+,FranG    INT
+,FranW    INT
+,FranL    INT
+,CareW    INT
+,CareL    INT
+,CareWP   FLOAT
+,POSeasG  INT
+,POSeasW  INT
+,POSeasL  INT
+,POFranG  INT
+,POFranW  INT
+,POFranL  INT
+,POCareG  INT
+,POCareW  INT
+,POCareL  INT
+,PRIMARY KEY(Name));
