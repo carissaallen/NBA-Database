bf520882ce4ee50b3b2e07504bef212576094543
Carissa Wood <carissawood@outlook.com>
2019-03-01 21:11:17 -0800

    Create Player_Stats table

---
+++ SQL/CreateTable_PlayerStats.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Team_Stats (
+TeamId    INT
+,G        INT
+,MP       INT
+,FG       INT
+,FGA      INT
+,FGP      FLOAT
+,ThreeP   INT
+,ThreePA  INT
+,ThreePP  FLOAT
+,TwoP     INT
+,TwoPA    INT
+,TwoPP    FLOAT
+,FT       INT
+,FTA      INT
+,FTP      FLOAT
+,ORB      INT
+,DRB      INT
+,TRB      INT
+,AST      INT
+,STL      INT
+,BLK      INT
+,TOV      INT
+,PF       INT
+,PTS      INT
+,PRIMARY KEY(TeamId));
