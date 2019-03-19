0120b11e659b964593e85157f23b61b476ac47b3
Carissa Allen <carissawood@outlook.com>
2019-03-18 23:03:39 -0700

    Update query

--- SQL/Queries/4_TopTenTeams_FGA.sql
+++ SQL/Queries/4_TopTenTeams_FGA.sql
@@ -1,6 +1,6 @@
 -- What are the top ten teams with the highest field goal attempts (FGA), in order from highest to lowest? 
 
-SELECT teamname FROM teams
+SELECT teamname, fga FROM teams
 JOIN team_stats ON teams.teamid = team_stats.teamid
 ORDER BY FGA DESC
 LIMIT 10;
