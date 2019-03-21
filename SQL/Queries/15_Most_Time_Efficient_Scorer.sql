b227ef3a299511c7ad2ae828839fe37978255b88
Mack Cooper <maqenzi@gmail.com>
2019-03-21 16:35:46 -0700

    Added Query Most Time Efficient Scorer

---
+++ SQL/Queries/15_Most_Time_Efficient_Scorer.sql
@@ -0,0 +1,7 @@
+SELECT player, pts, mp, ptsPer48
+FROM (
+	SELECT player, pts, mp, CAST(pts as FLOAT)*48/CAST(mp as FLOAT) as ptsPer48
+	FROM player_stats
+	WHERE pts > 0 AND mp > 0) as ps
+ORDER BY ptsPer48 DESC
+LIMIT 1;
