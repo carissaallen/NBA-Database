f55783dd443193984f53945267fe93dec852319b
Carissa Allen <carissawood@outlook.com>
2019-03-21 13:27:20 -0700

    Modify query to calculate eFG%

--- SQL/Queries/5_TopTenTeams_FGP.sql
+++ SQL/Queries/5_TopTenTeams_FGP.sql
@@ -1,6 +1,7 @@
 -- What are the top ten teams with the highest effective field goal percentages (EFG\%), in order from highest to lowest? 
 
-SELECT teamname, fgp FROM teams
+SELECT teamname, (fg + (0.5 * threeP))/fga as efg
+FROM teams
 JOIN team_stats ON teams.teamid = team_stats.teamid
-ORDER BY FGA DESC
+ORDER BY efg DESC
 LIMIT 10;
