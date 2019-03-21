3263fd7614f9e355617481b8d53cb04a2e8b2d09
Carissa Allen <carissawood@outlook.com>
2019-03-21 14:07:03 -0700

    Modify query to take teams' average free throws/game

--- SQL/Queries/9_TeamAvg_HighestFreeThrows.sql
+++ SQL/Queries/9_TeamAvg_HighestFreeThrows.sql
@@ -1,16 +1,7 @@
 -- On average, which teams scored the most free throws? 
 
-SELECT teamname, AVG(ft)
-FROM teams, team_stats
-WHERE teams.teamid = team_stats.teamid
-GROUP BY teamname
-ORDER BY AVG(ft) DESC
-FETCH FIRST 15 ROWS ONLY;
-
--- Alternative query
-
-SELECT teamname, ft
-FROM teams JOIN team_stats 
-ON teams.teamid = team_stats.teamid 
-ORDER BY ft DESC
+SELECT teamname, ft/g
+FROM teams
+JOIN team_stats ON teams.teamid = team_stats.teamid
+ORDER BY ft/g DESC
 FETCH FIRST 15 ROWS ONLY;
